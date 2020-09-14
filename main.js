f4d98f853a9e19fa7fd143707bb1a33d04e07b1f
natenadze1102 <natenadze1102@gmail.com>
2020-09-14 08:54:11 +0400

    reduce video-size

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 'use strict'
 
-function  myFunction () {
+function  myFunction() {
   
-  if(document.getElementById("myVideo-b").style.display == "none") {
-    document.getElementById('myVideo-b').style.display = "block";
-    document.getElementById('myVideo-n').style.display = "none";
+  if (document.getElementById("myVideo-b").style.display == "none") {
+        document.getElementById('myVideo-b').style.display = "block";
+        document.getElementById('myVideo-n').style.display = "none";
+        document.getElementById("clickBt").style.background='#8c7663';
   } else {
-    document.getElementById('myVideo-b').style.display = "none";
-    document.getElementById('myVideo-n').style.display = "block";
+        document.getElementById('myVideo-b').style.display = "none";
+        document.getElementById('myVideo-n').style.display = "block";
+        document.getElementById("clickBt").style.background='#312366';
   }
 }
 
-console.log(document.getElementById("myVideo-b").style.display == "none")
+
